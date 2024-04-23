e47f026009aae6e4e46f80409b5a0349645229b5
Álvaro Holguera <correo@laultimapregunta.com>
2024-04-23 11:18:01 +0200

    Primera clase. Declarar variables

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Programando</title>
+</head>
+
+<body>
+    
+    <h1 id="saludo"></h1>
+
+    
+    <script>
+
+        
+        
+        
+        
+        var saludo; 
+        
+        var alive = true;
+        
+        var x = "13";
+        var y = "2";
+        var resultado = x + y;
+        
+        console.log(resultado);
+        
+        
+        saludo = "Hola mundo";
+        
+        console.log(saludo);
+        
+        
+        saludo = "Adiós mundo cruel";
+        
+        console.log(saludo);
+        
+        
+        
+        //Esto para más adelante
+        //document.getElementById("saludo").innerHTML = "HOLA MUNDO";
+        
+        
+        
+    
+    </script>
+    
+</body>
+</html>
