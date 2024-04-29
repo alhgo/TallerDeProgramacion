8a87b56a0b0177062bac8d4a24c8cd16eb5b7d12
Álvaro Holguera <correo@laultimapregunta.com>
2024-04-29 11:06:14 +0200

    If + elseif

--- index.php
+++ index.php
@@ -5,17 +5,18 @@
 <title>Programando</title>
 </head>
 
-<body>
+<body onLoad="Saludar()">
     
     <h1 id="saludo"></h1>
 
     
+    <button onClick="RecibirImpacto()">IMPACTO</button>
     <script>
 
         var shield = 100;
         var lifes = 5;
         var damage;
-        var msg;
+        var msg = "COMIENZA EL JUEGO";
         var inv = false;
         var coolDown = 5; //Tiempo en segundos que tarda en quitarse el coolDown
         var alive = true; 
@@ -25,10 +26,12 @@
         function Saludar(){
             //msg = "Hola mundo desde una función";
             console.log(msg);
+            document.getElementById("saludo").innerHTML = msg;
         }
         
         function Impacto()
         {
+
             damage = Math.random() * 5000;
             damage = Math.floor(damage)/100;
             shield -= damage; //Este es lo mismo que shield = shield - damage;
@@ -44,7 +47,19 @@
             }
             else {
                 lifes--;
-                msg = "MUERTE!!!!";
+                if(lifes > 1){
+                    shield = 100;
+                    msg = "Te quedan " + lifes + " vidas. Y 100 de escudo"; 
+                }
+                else if(lifes == 1){
+                    shield = 100;
+                    msg = "Te queda 1 vida. Y 100 de escudo"; 
+                }
+                else {
+                    msg = "Estás MUERTOS";
+                    alive = false;
+                }
+                
                 
             }
             
@@ -56,10 +71,26 @@
             
         }
         
+        function RecibirImpacto()
+        {
+            //Ejemplo de comprobación con OR
+            /*
+            if(alive == false || inv == true){
+                console.log("No pasa nada");
+            }
+            */
+            
+            
+            
+            if(alive == true && inv == false){
+                Impacto();
+            }
+            else{
+                console.log("No se puede recibir impacto estando mierto")
+            }
+        }
         
-        Impacto();
-        Impacto();
-        Impacto();
+
         
         
 
